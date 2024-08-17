6d2df92bf6485b48e6969d8ab1bda5f20a3908a9
Msalem17 <muhammedsalem17@outlook.com>
2024-08-17 11:36:12 +0300

    update

--- app/components/Deals.tsx
+++ app/components/Deals.tsx
@@ -4,7 +4,7 @@ import Slider from "./Slider"
 
 const Deals = () => {
     return (
-        <div className=" relative sm:h-[820px] lg:h-[800px]">
+        <div className=" relative lg:h-[800px]">
             <Image className="w-full h-full rounded-xl object-cover opacity-100 "
                 src="https://forever.travel-assets.com/flex/flexmanager/mediaasset/714087-0_2-BEX_GO_Alps_Therin-00539RGB.jpg?impolicy=fcrop&w=1200&h=514&p=1&q=high"
                 alt="Scenic view" />
@@ -16,11 +16,11 @@ const Deals = () => {
                     <div>
                         <button className="bg-white text-sky-600 text-xl font-semibold p-4 rounded-full hidden lg:block" >See all deals</button>
                     </div>
-                </div>
-                <div className=" h-full w-full bg-[rgba(1,82,66,0.8)] ">
-                    <div className="flex">
-                        <Slider Image={Ads} />
-                    </div>
+                    {/* <div className="  "> */}
+                        <div className="flex h-full bg-[rgba(1,82,66,0.8)]">
+                            <Slider Image={Ads} />
+                        </div>
+                    {/* </div> */}
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ import { Discover } from "./components/Data";
 export default function Home() {
 
   return (
-    <div className="">
-        <div className="m-10">
-          <Trip />
-        </div>
-        <div>
-          <h5 className="text-3xl font-bold p-5">Discover your new favourite stay</h5>
-          <Swipper image={Discover.map((e) => (e.Img))} />
-        </div>
-        <div className="w-full">
-          <Deals />
-        </div>
+    <>
+      <div className="mx-10 my-5">
+        <Trip />
+      </div>
+      <div>
+        <h5 className="text-3xl font-bold p-5">Discover your new favourite stay</h5>
+        <Swipper image={Discover.map((e) => (e.Img))} />
+      </div>
+      <div className="w-full">
+        <Deals />
+      </div>
 
-    </div>
+    </>
   );
 }
